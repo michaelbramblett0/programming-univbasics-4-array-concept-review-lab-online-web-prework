6ca3bd15f77fe1335ffd51cf29e3dcef41b4bdba
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-09-30 15:48:24 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  find_element_index([1,2,3])
 end
 
 def find_max_value(array)
-  # Add your solution here
+  find_max_value
 end
 
 def find_min_value(array)
-  # Add your solution here
+find_min_value ([10,5,3,7,19])
 end
